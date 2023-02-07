5d74c09af0be5acf00404df0e30b02b9df039c61
Tolga <tolgaozgunn@gmail.com>
2023-02-07 20:42:17 +0000

    Add auth

--- backend/afetgonullu/settings.py
+++ backend/afetgonullu/settings.py
@@ -43,6 +43,16 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
 INSTALLED_APPS = [
     'rest_framework',
     'locations',
--- backend/locations/views.py
+++ backend/locations/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated 
 from .models import Location
 from .serializers import LocationSerializer
 
 class LocationList(generics.ListCreateAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    permission_classes = [IsAuthenticated]
