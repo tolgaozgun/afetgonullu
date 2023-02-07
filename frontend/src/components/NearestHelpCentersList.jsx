332fe211f1cb1f9dbf1cec573994b64c9e9e1d19
Selim Can Guler <selimcangler@gmail.com>
2023-02-08 00:07:45 +0300

    city list data

--- frontend/src/components/NearestHelpCentersList.jsx
+++ frontend/src/components/NearestHelpCentersList.jsx
@@ -2,10 +2,16 @@ import { Box, Button, TextField } from "@mui/material"
 import { useState } from "react"
 
 const NearestHelpCentersList = ({userPosition, helpCenterPositions}) => {
-    const [currentLocation, setCurrentLocation] = useState('')
+    const [currentLocation, setCurrentLocation] = useState(null)
+    const [error, setError] = useState(null)
     
 	const handleList = () => {
         const getNearestHelpCenters = async () => {
+            if (!currentLocation) {
+                setError("Şehir boş bırakılamaz")
+                return
+            }
+
             const url = `http://nominatim.openstreetmap.org/search?city={currentLocation}&format=json`
             const response = await fetch(url)
             const data = await response.json()
@@ -19,6 +25,7 @@ const NearestHelpCentersList = ({userPosition, helpCenterPositions}) => {
     return (
         <Box>
             <TextField
+                error={error}
                 label="Şehir"
                 value={currentLocation}
                 onChange={setCurrentLocation}
