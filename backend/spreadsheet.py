31ef504cd03030eb4681ca8d8911d72c93a1674d
ismail-deniz <ismaildenizcs@gmail.com>
2023-02-08 16:31:48 +0300

    successful parsing

--- backend/spreadsheet.py
+++ backend/spreadsheet.py
@@ -24,6 +24,34 @@ import gspread
 from gspread_dataframe import set_with_dataframe
 import openpyxl 
 
+def get_color(rgb):
+    if rgb == "FF00FF00":
+        return "Green"
+    elif rgb == "FF999999":
+        return "Grey"
+    elif rgb == "FFFF9900":
+        return "Orange"
+    elif rgb == "FFFFFF00":
+        return "Yellow"
+    elif rgb == "FF0000FF":
+        return "Blue"
+    elif rgb == "FFFF0000":
+        return "Red"
+
+def get_severity(rgb):
+    if rgb == "FF0000FF":
+        return 0
+    elif rgb == "FF999999":
+        return 1
+    elif rgb == "FF00FF00":
+        return 2
+    elif rgb == "FFFFFF00":
+        return 3
+    elif rgb == "FFFF9900":
+        return 4
+    elif rgb == "FFFF0000":
+        return 5
+
 # Use the credentials to authenticate with the Google API
 gc = gspread.service_account(filename='./credentials.json')
 
@@ -41,17 +69,41 @@ workbook = openpyxl.reader.excel.load_workbook('temp.xlsx')
 # Get the first sheet of the Workbook
 worksheet = workbook.worksheets[0]
 
+# Find the names of places
+name_place = []
+
+r = 2
+cell = worksheet.cell(row=r, column=1)
+value = cell.value.strip()
+name_place.append(value)
+while value != None:
+    r += 1
+    cell = worksheet.cell(row=r, column=1)
+    value = cell.value
+    name_place.append(value)
+    print(value)
+
 colors_D = []
 colors_E = []
+sev_D = []
+sev_E = []
 
 # Loop through the cells to extract the cell background color information
-for row in range(2, 23):
+for row in range(2, r):
     cell = worksheet.cell(row=row, column=4)
-    colors_D.append(cell.fill.start_color.index)
+    colors_D.append(get_color(cell.fill.start_color.index))
+    sev_D.append(get_severity(cell.fill.start_color.index))
+    
 
-for row in range(2, 23):
+for row in range(2, r):
     cell = worksheet.cell(row=row, column=5)
-    colors_E.append(cell.fill.start_color.index)
+    colors_E.append(get_color(cell.fill.start_color.index))
+    sev_E.append(get_severity(cell.fill.start_color.index))
 
-print('Colors of cells D2:D22:', colors_D)
-print('Colors of cells E2:E22:', colors_E)
+result_malzeme = dict(zip(name_place, sev_D))
+result_insan = dict(zip(name_place, sev_E))
+
+print("Malzeme:\n", result_malzeme)
+print("Insan:\n", result_insan)
+print(len(result_malzeme))
+
