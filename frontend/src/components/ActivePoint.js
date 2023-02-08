2d9b79c8e670190ed1f916eb41f9f5967681f07e
Selim Can Guler <selimcangler@gmail.com>
2023-02-08 17:16:44 +0300

    yol tarifi & paylas

--- frontend/src/components/ActivePoint.js
+++ frontend/src/components/ActivePoint.js
@@ -17,11 +17,21 @@ import { Link } from "react-router-dom";
             {props.needs_people ? <Typography fontSize={16}>Gönüllü lazım</Typography> : <Typography fontSize={16}>Gönüllü lazım değil</Typography>}
           </Grid>
           <Grid item>
-            {props.needs_donation ? <Typography fontSize={16}>Bağış lazım</Typography> : <Typography fontSize={16}>Bağış lazım değil</Typography> }
+            {props.needs_donation ? 
+              <Typography color={props.needs_donation ? "red" : "green"} fontSize={16}>Bağış lazım</Typography> 
+                : <Typography 
+                    fontSize={16}
+                    color={props.needs_donation ? "red" : "green"}
+                  >
+                    Bağış lazım değil
+                  </Typography> 
+              }
           </Grid>
         </Grid>
         <Grid item>
-          {props.help_message && <Typography variant="body2">{props.help_message}</Typography>}
+          {props.help_message && 
+            <Typography variant="body2" style={{whiteSpace: 'pre-line'}}>{props.help_message}</Typography>
+          }
         </Grid>
         <Grid item container direction="column" alignItems="center" spacing={2}>
           <Grid item>
