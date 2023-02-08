6a853247765e83bc69adc5ec2d8e7d4ec40e5c44
Selim Can Guler <selimcangler@gmail.com>
2023-02-08 20:58:47 +0300

    user operations

--- frontend/src/pages/UserOperations.jsx
+++ frontend/src/pages/UserOperations.jsx
@@ -1,13 +1,25 @@
+import { DataGrid } from "@mui/x-data-grid";
 import { useEffect } from "react";
 
 const UserOperations = () => {
+    const rows = [
+        {}
+    ]
+    const columns = [
+        { field: "E-mail", header: "E-mail", width: 150 },
+        { field: "Per"}
+    ]
 
     useEffect(() => {
 
     }, [])
 
     return (
-        
+        <DataGrid
+
+        >
+
+        </DataGrid>
     );
 }
  
