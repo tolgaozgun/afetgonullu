0bdc9530e5db128ea9748de314ab2946b774c867
Tolga <tolgaozgunn@gmail.com>
2023-02-08 11:55:59 +0000

    Change React UI

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -11,28 +11,6 @@ function Map({ markingPoints, mapView, center }) {
     // markingPoints is an 
     const [activePoint, setActivePoint] = useState(null)
 
-    markingPoints = [{
-        "id": 4,
-        "name": "Gulcimen Aspava",
-        "latitude": 39.9276529,
-        "longitude": 32.8094168,
-        "needs_people": true,
-        "needs_donation": true,
-        "help_message": "Yardim",
-        "latest_information_date": "2023-02-07T22:50:47.893307Z",
-        "geometry": {
-            "lat": 39.9276529,
-            "lon": 32.8094168
-        },
-        "help": {
-            "needed": true,
-            "message": "Yardim"
-        },
-        "properties": {
-            "name": "Gulcimen Aspava"
-        }
-    }]
-
     return (
         <MapContainer center={center} zoom={7} zoomSettings={{ enable: true, toolbars: ['Zoom', 'ZoomIn', 'ZoomOut', 'Pan', 'Reset'] }}>
             <TileLayer
